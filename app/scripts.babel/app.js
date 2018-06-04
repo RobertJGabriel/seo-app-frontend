fa89f46e56611250896ce1f4df6eae0ce76f658a
Robert James Gabriel <robert_gabriel@outlook.com>
2018-06-03 22:30:37 -0500

    meesages

--- app/scripts.babel/app.js
+++ app/scripts.babel/app.js
@@ -1,10 +1,12 @@
 'use strict';
 
-
 const getSEOReportURL = 'https://seo-bot-.herokuapp.com/';
 
 
-var vm = new Vue({
+
+
+
+const vm = new Vue({
   el: '#app',
   data: {
     features: [],
@@ -17,81 +19,82 @@ var vm = new Vue({
     errorMessage: '',
     successMessage: '',
     email: '',
+    license: null,
     URL: ''
   },
 
   methods: {
-    updateURL: function (arrayOfTabs) {
+    updateURL: function updateURL(arrayOfTabs) {
       var activeTab = arrayOfTabs[0];
       this.URL = activeTab.url;
     },
-    goHome: function () {
+    updateLicense: function updateLicense(result) {
+      this.license = result.license;
+
+    },
+    goHome() {
       this.features = [];
       this.loading = null;
       this.success = false;
       this.error = false;
     },
-    share: function () {
-      var opened = this.forwordOpen;
+    share() {
+      const opened = this.forwordOpen;
       if (opened) {
         this.forwordOpen = false;
       } else {
         this.forwordOpen = true;
       }
-
     },
-    goTo: function (URL) {
+    goTo(URL) {
       console.log(URL);
-      var win = window.open(URL);
+      const win = window.open(URL);
       win.focus();
     },
-    validateUrl: function (url) {
-      var regexp = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+    validateUrl(url) {
+      const regexp = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
       return regexp.test(url);
     },
-    validateEMAIL: function (email) {
-      var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    validateEMAIL(email) {
+      const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
       return regex.test(email);
     },
-    removeDuplicates: function (arr) {
-      let unique_array = []
+    removeDuplicates(arr) {
+      const unique_array = [];
       for (let i = 0; i < arr.length; i++) {
         if (unique_array.indexOf(arr[i]) == -1) {
-          unique_array.push(arr[i])
+          unique_array.push(arr[i]);
         }
       }
-      return unique_array
+      return unique_array;
     },
-    sortFeatures: function (features) {
-
-      for (var i = 0; i < features.length; i++) {
-        var obj = features[i];
+    sortFeatures(features) {
+      for (let i = 0; i < features.length; i++) {
+        const obj = features[i];
         if (!obj.supported) {
           console.log(obj.name);
           this.notSupport.push(obj.name);
         }
       }
       this.notSupport = this.removeDuplicates(this.notSupport);
-
     },
-    sendEmailReport: function (event) {
-
+    sendEmailReport(event) {
       this.error = false;
       this.success = false;
       this.sortFeatures(this.features);
-      let encodedEmail = encodeURI(this.email); // Encode the url to https safe.
+      const encodedEmail = encodeURI(this.email); // Encode the url to https safe.
 
       if (this.notSupport.length == 0 || this.notSupport === undefined) {
         this.error = true;
-        this.errorMessage = `Theres no errors to send woooooooo!`;
-        return false
-      }
-
-      if (event.key != 'Enter'){
+        this.errorMessage = 'Theres no errors to send woooooooo!';
         return false;
       }
 
-      if (this.validateEMAIL(this.email) == false || this.email == '' ) {
+      if (event.key != 'Enter') {
+        return false;
+      }
+
+      if (this.validateEMAIL(this.email) == false || this.email == '') {
         this.errorMessage = 'Please enter a valid email';
         this.error = true;
         this.loading = false;
@@ -100,31 +103,34 @@ var vm = new Vue({
 
       this.loading = true;
 
-
-      let object = {
-        email : this.email,
-        url : this.URL,
-        notSupport : this.notSupport
+      const object = {
+        email: this.email,
+        url: this.URL,
+        notSupport: this.notSupport
       };
 
-      this.$http.post(`${getSEOReportURL}email?email=${encodedEmail}`, object).then(response => {
-        this.success = true;
-        this.successMessage = `Report sent to ${this.email}`;
-        this.loading = false;
-      }, response => {
-        this.error = true;
-        this.loading = null;
-        this.errorMessage = `Something went wrong sending the email report to ${this.email}`;
-      });
-
-
+      this.$http
+        .post(`${getSEOReportURL}email?email=${encodedEmail}`, object)
+        .then(
+          response => {
+            this.success = true;
+            this.successMessage = `Report sent to ${this.email}`;
+            this.loading = false;
+          },
+          response => {
+            this.error = true;
+            this.loading = null;
+            this.errorMessage = `Something went wrong sending the email report to ${
+              this.email
+            }`;
+          }
+        );
     },
-    getSEOReport: function (event) {
-
+    getSEOReport(event) {
       this.error = false; // Set the error to false.
       this.features = []; // Clear features
 
-      let encodedURL = encodeURI(this.URL); // Encode the url to https safe.
+      const encodedURL = encodeURI(this.URL); // Encode the url to https safe.
 
       if (this.loading || this.URL == '' || event.key != 'Enter') {
         return false;
@@ -139,25 +145,32 @@ var vm = new Vue({
 
       this.loading = true;
 
-      this.$http.get(`${getSEOReportURL}results?name=${encodedURL}`).then(response => {
-        this.features = JSON.parse(response.bodyText).features.css; // Parse the coffee lists
-        this.loading = false;
-      }, response => {
-        this.error = true;
-        this.loading = null;
-        this.features = [];
-        this.errorMessage = 'Opps, something seems to have gone wrong. Please try again';
-      });
+      this.$http.get(`${getSEOReportURL}results?name=${encodedURL}`).then(
+        response => {
+          this.features = JSON.parse(response.bodyText).features.css; // Parse the coffee lists
+          this.loading = false;
+        },
+        response => {
+          this.error = true;
+          this.loading = null;
+          this.features = [];
+          this.errorMessage =
+            'Opps, something seems to have gone wrong. Please try again';
+        }
+      );
     }
   }
 });
 
 // Set config settings
 Vue.config.productionTip = false;
-Vue.config.devtools = false
-
+Vue.config.devtools = false;
 
 chrome.tabs.query({
-  active: true,
-  currentWindow: true
-}, vm.updateURL.bind(this));
+    active: true,
+    currentWindow: true
+  },
+  vm.updateURL.bind(this)
+);
+
+chrome.storage.sync.get(['license'], vm.updateLicense.bind(this));
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -10,84 +10,84 @@ import {
 const $ = gulpLoadPlugins();
 
 gulp.task('extras', () => {
-  return gulp.src([
-    'app/*.*',
-    'app/scripts/**/*.js',
-    'app/styles/**/*.min.css',
-    'app/_locales/**',
-    '!app/scripts.babel',
-    '!app/*.json',
-    '!app/*.html'
-  ], {
-    base: 'app',
-    dot: true
-  }).pipe(gulp.dest('dist'));
+	return gulp.src([
+		'app/*.*',
+		'app/scripts/**/*.js',
+		'app/styles/**/*.min.css',
+		'app/_locales/**',
+		'!app/scripts.babel',
+		'!app/*.json',
+		'!app/*.html'
+	], {
+		base: 'app',
+		dot: true
+	}).pipe(gulp.dest('dist'));
 });
 
 function lint(files, options) {
-  return () => {
-    return gulp.src(files)
+	return () => {
+		return gulp.src(files)
       .pipe($.eslint(options))
       .pipe($.eslint.format());
-  };
+	};
 }
 
 gulp.task('lint', lint('app/scripts.babel/*.js', {
-  env: {
-    es6: true
-  }
+	env: {
+		es6: true
+	}
 }));
 
 gulp.task('images', () => {
-  return gulp.src('app/images/**/*')
+	return gulp.src('app/images/**/*')
     .pipe($.if($.if.isFile, $.cache($.imagemin({
-        progressive: true,
-        interlaced: true,
+	progressive: true,
+	interlaced: true,
         // Don't remove IDs from SVGs, they are often used
         // as hooks for embedding and styling
-        svgoPlugins: [{
-          cleanupIDs: false
-        }]
-      }))
+	svgoPlugins: [{
+		cleanupIDs: false
+	}]
+}))
       .on('error', function (err) {
-        console.log(err);
-        this.end();
-      })))
+	console.log(err);
+	this.end();
+})))
     .pipe(gulp.dest('dist/images'));
 });
 
 gulp.task('html', () => {
-  return gulp.src('app/*.html')
+	return gulp.src('app/*.html')
     .pipe($.useref({
-      searchPath: ['.tmp', 'app', '.']
-    }))
+	searchPath: ['.tmp', 'app', '.']
+}))
     .pipe($.sourcemaps.init())
     .pipe($.if('*.js', $.uglify()))
     .pipe($.if('*.css', $.cleanCss({
-      compatibility: '*'
-    })))
+	compatibility: '*'
+})))
     .pipe($.sourcemaps.write())
     .pipe($.if('*.html', $.htmlmin({
-      removeComments: true,
-      collapseWhitespace: true
-    })))
+	removeComments: true,
+	collapseWhitespace: true
+})))
     .pipe(gulp.dest('dist'));
 });
 
 gulp.task('sass', () => {
-  return gulp.src('app/styles/sass/styles.sass')
+	return gulp.src('app/styles/sass/styles.sass')
     .pipe($.sass().on('error', sass.logError))
     .pipe(gulp.dest('dist/styles/'));
 });
 
 gulp.task('chromeManifest', () => {
-  return gulp.src('app/manifest.json')
+	return gulp.src('app/manifest.json')
     .pipe($.chromeManifest({
-      buildnumber: true
-    }))
+	buildnumber: true
+}))
     .pipe($.if('*.css', $.cleanCss({
-      compatibility: '*'
-    })))
+	compatibility: '*'
+})))
     .pipe($.if('*.js', $.sourcemaps.init()))
     .pipe($.if('*.js', $.uglify()))
     .pipe($.if('*.js', $.sourcemaps.write('.')))
@@ -95,65 +95,63 @@ gulp.task('chromeManifest', () => {
 });
 
 gulp.task('babel', () => {
-  return gulp.src('app/scripts.babel/*.js')
+	return gulp.src('app/scripts.babel/*.js')
     .pipe($.babel({
-      presets: ['es2015']
-    }))
+	presets: ['es2015']
+}))
     .pipe(gulp.dest('app/scripts'));
 });
 
-
 gulp.task('thirdparty', () => {
-  return gulp.src('app/scripts.babel/vendor/*.js')
+	return gulp.src('app/scripts.babel/vendor/*.js')
     .pipe(gulp.dest('app/scripts/vendor/'));
 });
-
 
 gulp.task('clean', del.bind(null, ['.tmp', 'dist']));
 
 gulp.task('watch', ['lint', 'babel'], () => {
-  $.livereload.listen();
+	$.livereload.listen();
 
-  gulp.watch([
-    'app/*.html',
-    'app/scripts/**/*.js',
-    'app/images/**/*',
-    'app/styles/**/*',
-    'app/_locales/**/*.json'
-  ]).on('change', $.livereload.reload);
+	gulp.watch([
+		'app/*.html',
+		'app/scripts/**/*.js',
+		'app/images/**/*',
+		'app/styles/**/*',
+		'app/_locales/**/*.json'
+	]).on('change', $.livereload.reload);
 
-  gulp.watch('app/scripts.babel/*.js', ['lint', 'babel']);
-  gulp.watch('bower.json', ['wiredep']);
+	gulp.watch('app/scripts.babel/*.js', ['lint', 'babel']);
+	gulp.watch('bower.json', ['wiredep']);
 });
 
 gulp.task('size', () => {
-  return gulp.src('dist/**/*').pipe($.size({
-    title: 'build',
-    gzip: true
-  }));
+	return gulp.src('dist/**/*').pipe($.size({
+		title: 'build',
+		gzip: true
+	}));
 });
 
 gulp.task('wiredep', () => {
-  gulp.src('app/*.html')
+	gulp.src('app/*.html')
     .pipe(wiredep({
-      ignorePath: /^(\.\.\/)*\.\./
-    }))
+	ignorePath: /^(\.\.\/)*\.\./
+}))
     .pipe(gulp.dest('app'));
 });
 
 gulp.task('package', () => {
-  const manifest = require('./dist/manifest.json');
-  return gulp.src('dist/**')
-    .pipe($.zip('chrome teamwork-' + manifest.version + '.zip'))
+	const manifest = require('./dist/manifest.json');
+	return gulp.src('dist/**')
+    .pipe($.zip('chrome -' + manifest.version + '.zip'))
     .pipe(gulp.dest('package'));
 });
 
 gulp.task('build', cb => {
-  runSequence(
+	runSequence(
     'lint', 'babel', 'chromeManifest', 'thirdparty', ['html', 'images', 'sass', 'extras'],
     'size', cb);
 });
 
 gulp.task('default', ['clean'], cb => {
-  runSequence('build', cb);
+	runSequence('build', cb);
 });
